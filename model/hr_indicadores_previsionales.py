02489f19ca1ae6362fdd3fa1c9344e958c6fe091
Cesar250101 <cesar250101@outlook.com>
2022-08-29 16:42:41 -0400

    API indicadores previsionales

--- model/hr_indicadores_previsionales.py
+++ model/hr_indicadores_previsionales.py
@@ -1,3 +1,5 @@
+import json
+from lib2to3.pytree import convert
 from odoo import api, fields, models, tools, _
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -238,101 +240,104 @@ class hr_indicadores_previsionales(models.Model):
             self.tope_imponible_salud=7
 
 
-        try:
-            html_doc = urlopen('https://www.previred.com/web/previred/indicadores-previsionales').read()
-            soup = BeautifulSoup(html_doc, 'html.parser')
+        # try:
+        #     html_doc = urlopen('https://www.previred.com/web/previred/indicadores-previsionales').read()
+        #     soup = BeautifulSoup(html_doc, 'html.parser')
 
-            letters = soup.find_all("table")
+        #     letters = soup.find_all("table")
 
-            def clear_string(cad):
-                cad = cad.replace(".", '').replace("$", '').replace(" ", '')
-                cad = cad.replace("Renta", '').replace("<", '').replace(">", '')
-                cad = cad.replace("=", '').replace("R", '').replace("I", '').replace("%", '')
-                cad = cad.replace(",", '.')
-                cad = cad.replace("1ff8","")
-                return cad
-        except ValueError:
-            return ""
+        #     def clear_string(cad):
+        #         cad = cad.replace(".", '').replace("$", '').replace(" ", '')
+        #         cad = cad.replace("Renta", '').replace("<", '').replace(">", '')
+        #         cad = cad.replace("=", '').replace("R", '').replace("I", '').replace("%", '')
+        #         cad = cad.replace(",", '.')
+        #         cad = cad.replace("1ff8","")
+        #         return cad
+        # except ValueError:
+        #     return ""
 
         def string_divide(cad, cad2, rounded):
             return round(float(cad) / float(cad2), rounded)
 
-
+        url = "https://api.gael.cloud/general/public/previred/082022"
+        payload={}
+        headers = {}
+        dict={}
+        data = requests.request("GET", url, headers=headers, data=payload)
+        dict = json.loads(data.text)
+        print(float(dict['UFValPeriodo'].replace(",",".")))
         try:
             # UF
-            self.uf = clear_string(letters[0].select("strong")[1].get_text())
+            self.uf =float(dict['UFValPeriodo'].replace(",",".")) 
 
             # 1 UTM
-            self.utm = clear_string(letters[1].select("strong")[3].get_text())
+            self.utm = int(dict['UTMVal'].replace(",00",""))
 
             # 1 UTA
-            self.uta = clear_string(letters[1].select("strong")[4].get_text())
+            self.uta = int(dict['UTAVal'].replace(",00",""))
 
             # 3 RENTAS TOPES IMPONIBLES UF
-            texto=letters[2].select("strong")[1]
-            texto2=letters[2].select("strong")[3]
-            self.tope_imponible_afp = string_divide(clear_string(letters[2].select("strong")[1].get_text()), self.uf, 2)
-            self.tope_imponible_ips = string_divide(clear_string(letters[2].select("strong")[3].get_text()), self.uf, 2)
-            self.tope_imponible_seguro_cesantia = string_divide(clear_string(letters[2].select("strong")[3].get_text()),
-                                                                self.uf, 2)
+            self.tope_imponible_afp = int(dict['RTIAfpUF'].replace(",","."))
+            self.tope_imponible_ips = int(dict['RTIIpsUF'].replace(",","."))
+            self.tope_imponible_seguro_cesantia = int(dict['RTISegCesUF'].replace(",","."))
 
             # 4 RENTAS TOPES IMPONIBLES
-            self.sueldo_minimo = clear_string(letters[3].select("strong")[1].get_text())
-            self.sueldo_minimo_otro = clear_string(letters[3].select("strong")[2].get_text())
+            self.sueldo_minimo = dict['RMITrabDepeInd'].replace(",",".")
+            self.sueldo_minimo_otro = dict['RMIMen18May65'].replace(",",".")
 
             # Ahorro Previsional Voluntario
-            self.tope_mensual_apv = string_divide(clear_string(letters[4].select("strong")[2].get_text()), self.uf, 2)
-            self.tope_anual_apv = string_divide(clear_string(letters[4].select("strong")[1].get_text()), self.uf, 2)
+            self.tope_mensual_apv = int(dict['APVTopeMensUF'].replace(",","."))
+            self.tope_anual_apv = int(dict['APVTopeAnuUF'].replace(",","."))
 
             # 5 DEPÓSITO CONVENIDO
-            self.deposito_convenido = string_divide(clear_string(letters[5].select("strong")[1].get_text()), self.uf, 2)
+            self.deposito_convenido = int(dict['DepConvenidoUF'].replace(",","."))
 
             # 6 RENTAS TOPES IMPONIBLES
-            self.contrato_plazo_indefinido_empleador = clear_string(letters[6].select("strong")[5].get_text())
-            self.contrato_plazo_indefinido_trabajador = clear_string(letters[6].select("strong")[6].get_text())
-            self.contrato_plazo_fijo_empleador = clear_string(letters[6].select("strong")[7].get_text())
-            self.contrato_plazo_indefinido_empleador_otro = clear_string(letters[6].select("strong")[9].get_text())
+            self.contrato_plazo_indefinido_empleador = dict['AFCCpiEmpleador'].replace(",",".")
+            self.contrato_plazo_indefinido_trabajador = dict['AFCCpiTrabajador'].replace(",",".")
+            self.contrato_plazo_fijo_empleador = dict['AFCCpfEmpleador'].replace(",",".")
+            self.contrato_plazo_indefinido_empleador_otro = dict['AFCCpi11Empleador'].replace(",",".")
 
             try:
                 # 7 ASIGNACIÓN FAMILIAR
-                self.asignacion_familiar_monto_a = clear_string(letters[9].select("td")[6].get_text())
-                self.asignacion_familiar_monto_b = clear_string(letters[9].select("td")[8].get_text())
-                self.asignacion_familiar_monto_c = clear_string(letters[9].select("td")[10].get_text())
+                self.asignacion_familiar_monto_a = int(dict['AFamTramoAMonto'].replace(",","."))
+                self.asignacion_familiar_monto_b = int(dict['AFamTramoBMonto'].replace(",","."))
+                self.asignacion_familiar_monto_c = int(dict['AFamTramoCMonto'].replace(",","."))
 
-                self.asignacion_familiar_primer = clear_string(letters[9].select("td")[7].get_text())[1:]
-                self.asignacion_familiar_segundo = clear_string(letters[9].select("td")[9].get_text())[6:]
-                self.asignacion_familiar_tercer = clear_string(letters[9].select("td")[11].get_text())[6:]
+                self.asignacion_familiar_primer = int(dict['AFamTramoAHasta'].replace(",","."))
+                self.asignacion_familiar_segundo = int(dict['AFamTramoBHasta'].replace(",","."))
+                self.asignacion_familiar_tercer = int(dict['AFamTramoCHasta'].replace(",","."))
             except:
                 pass
             # 8 TASA COTIZACIÓN OBLIGATORIO AFP
-            self.tasa_afp_capital = clear_string(letters[8].select("strong")[8].get_text())
-            self.tasa_sis_capital = clear_string(letters[8].select("strong")[9].get_text())
+            self.tasa_afp_capital = dict['AFPCapitalTasaDep'].replace(",",".")
+            self.tasa_sis_capital = dict['AFPCapitalTasaSIS'].replace(",",".")
 
-            self.tasa_afp_cuprum = clear_string(letters[8].select("strong")[11].get_text().replace(" ", '').replace("%", '').replace("1ff8", ''))
-            self.tasa_sis_cuprum = clear_string(letters[8].select("strong")[12].get_text())
+            self.tasa_afp_cuprum = dict['AFPCuprumTasaDep'].replace(",",".")
+            self.tasa_sis_cuprum = dict['AFPCuprumTasaSIS'].replace(",",".")
 
-            self.tasa_afp_habitat = clear_string(letters[8].select("strong")[14].get_text())
-            self.tasa_sis_habitat = clear_string(letters[8].select("strong")[15].get_text())
+            self.tasa_afp_habitat = dict['AFPHabitatTasaDep'].replace(",",".")
+            self.tasa_sis_habitat = dict['AFPHabitatTasaSIS'].replace(",",".")
 
-            self.tasa_afp_planvital = clear_string(letters[8].select("strong")[17].get_text())
-            self.tasa_sis_planvital = clear_string(letters[8].select("strong")[18].get_text())
+            self.tasa_afp_planvital = dict['AFPPlanVitalTasaDep'].replace(",",".")
+            self.tasa_sis_planvital = dict['AFPPlanVitalTasaSIS'].replace(",",".")
 
-            self.tasa_afp_provida = clear_string(letters[8].select("strong")[20].get_text().replace(" ", '').replace("%", '').replace("1ff8", ''))
-            self.tasa_sis_provida = clear_string(letters[8].select("strong")[21].get_text())
+            self.tasa_afp_provida = dict['AFPProVidaTasaDep'].replace(",",".")
+            self.tasa_sis_provida = dict['AFPProVidaTasaSIS'].replace(",",".")
 
-            self.tasa_afp_modelo = clear_string(letters[8].select("strong")[23].get_text())
-            self.tasa_sis_modelo = clear_string(letters[8].select("strong")[24].get_text())
+            self.tasa_afp_modelo = dict['AFPModeloTasaDep'].replace(",",".")
+            self.tasa_sis_modelo = dict['AFPModeloTasaSIS'].replace(",",".")
 
-            self.tasa_afp_uno = clear_string(letters[8].select("strong")[23].get_text())
-            self.tasa_sis_uno = clear_string(letters[8].select("strong")[24].get_text())
+            self.tasa_afp_uno = dict['AFPUnoTasaDep'].replace(",",".")
+            self.tasa_sis_uno = dict['AFPUnoTasaSIS'].replace(",",".")
 
-            self.tasa_independiente_capital = clear_string(letters[8].select("strong")[10].get_text())[:5]
-            self.tasa_independiente_cuprum = clear_string(letters[8].select("strong")[13].get_text())
-            self.tasa_independiente_habitat = clear_string(letters[8].select("strong")[16].get_text())
-            self.tasa_independiente_planvital = clear_string(letters[8].select("strong")[19].get_text())
-            self.tasa_independiente_provida = clear_string(letters[8].select("strong")[22].get_text())
-            self.tasa_independiente_modelo = clear_string(letters[8].select("strong")[25].get_text())
-            self.tasa_independiente_uno = clear_string(letters[8].select("strong")[25].get_text())
+            self.tasa_independiente_capital = dict['AFPCapitalTasaInd'].replace(",",".")
+            self.tasa_independiente_cuprum = dict['AFPCuprumTasaInd'].replace(",",".")
+            self.tasa_independiente_habitat = dict['AFPHabitatTasaInd'].replace(",",".")
+            self.tasa_independiente_planvital = dict['AFPPlanVitalTasaInd'].replace(",",".")
+            self.tasa_independiente_provida = dict['AFPProVidaTasaInd'].replace(",",".")
+            self.tasa_independiente_modelo = dict['AFPModeloTasaInd'].replace(",",".")
+            self.tasa_independiente_uno = dict['AFPUnoTasaInd'].replace(",",".")
 
         except ValueError:
             return ""
